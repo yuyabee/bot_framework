02459bc2f8f0dfb1634ed5d67df8ab573d85f775
Yuya Yabe <yuyabee@gmail.com>
2016-08-03 03:37:39 +0000

    Update attachment model

--- lib/bot_framework/models/attachment.ex
+++ lib/bot_framework/models/attachment.ex
@@ -1,8 +1,8 @@
 defmodule BotFramework.Models.Attachment do
-  defstruct [:contentType, :content]
+  defstruct [:contentType, :content, :contentUrl]
 
   @type t :: %__MODULE__{
-    contentType: String.t content: any
+    contentType: String.t content: any, contentUrl: String.t
   }
 
   def decoding_map, do: %__MODULE__{}
